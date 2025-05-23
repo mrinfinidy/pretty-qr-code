9e90e6e45b16e14aad9dc09ad0ef90b7cdb53e78
afkdev8 <mr.infinidy@protonmail.com>
2025-05-23 05:19:24 +0200

    Adjust setup.py to import module to entrypoint

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
-#!/usr/bin/env python
-
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='qr-code-generator',
     version='1.0',
-    author = "Amon Felbermayer",
-    author_email = "amon.felbermayer@nesto-software.de",
-    description = ("A tool which generates beautiful nesto-branded qr-codes."),
-    license = "Closed",
-    url = "https://gitlab.nesto.app/nesto-software/pos-adapter-v2/qr-code-generator",
-    packages=find_packages(),
-    scripts = ["src/qr_code_generator.py"],
+    author="Amon Felbermayer",
+    author_email="amon.felbermayer@nesto-software.de",
+    description="A tool which generates beautiful nesto-branded qr-codes.",
+    license="Closed",
+    url="https://gitlab.nesto.app/nesto-software/pos-adapter-v2/qr-code-generator",
+    package_dir={"": "src"},
+    py_modules=["qr_code_generator", "entrypoint"],
+    scripts=["src/entrypoint.py"],
     install_requires=[
         'qrcode',
         'PIL'
-    ]
+    ],
 )
