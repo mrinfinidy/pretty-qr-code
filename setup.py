bbdbf724c6bce0845d2b3f7cbcdb1cd2c6958aa2
afkdev8 <mr.infinidy@protonmail.com>
2026-01-29 03:04:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 setup(
-    name='qr-code-generator',
+    name='pretty-qr-code',
     version='1.0',
-    author="Amon Felbermayer",
-    author_email="amon.felbermayer@nesto-software.de",
-    description="A tool which generates beautiful nesto-branded qr-codes.",
-    license="Closed",
-    url="https://gitlab.nesto.app/nesto-software/pos-adapter-v2/qr-code-generator",
+    author="afkdev8",
+    author_email="mail@afkdev8.com",
+    description="A tool which generates pretty QR codes.",
+    license="MIT",
+    url="https://github.com/mrinfinidy/pretty-qr-code",
     package_dir={"": "src"},
     py_modules=["qr_code_generator", "entrypoint", "const"],
     install_requires=[
         'qrcode',
-        'PIL'
+        'pillow'
     ],
     entry_points={
         'console_scripts': [
